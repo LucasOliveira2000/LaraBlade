801b5311ea5fe453886c6d0da28bbe16d0dca1a6
LucasOliveira2000 <j-lucas9@hotmail.com>
2024-07-31 21:53:59 -0300

    build2
    build2

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -6,6 +6,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>@yield('title', 'My Laravel App')</title>
    @vite(['resources/css/app.css', 'resources/js/app.js'])
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body style="background: url('{{ asset('back1.png') }}')">
     <nav class="navbar navbar-expand-lg p-3 border border-3 rounded-bottom-5 bg-dark justify-content-center">
