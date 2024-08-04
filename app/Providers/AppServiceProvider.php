6a22fa685bd439c084960d7a482accc845a0a86c
LucasOliveira2000 <j-lucas9@hotmail.com>
2024-08-04 20:45:07 -0300

    forçando https
    forçando https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+
+            // Adiciona cabeçalhos para arquivos estáticos
+            \Illuminate\Support\Facades\Response::macro('css', function ($path) {
+                return response()->file($path, [
+                    'Content-Type' => 'text/css; charset=UTF-8',
+                    'X-Content-Type-Options' => 'nosniff',
+                ]);
+            });
+
+            \Illuminate\Support\Facades\Response::macro('js', function ($path) {
+                return response()->file($path, [
+                    'Content-Type' => 'application/javascript; charset=UTF-8',
+                    'X-Content-Type-Options' => 'nosniff',
+                ]);
+            });
+        }
+
         Blade::component('components.toast', 'toast');
     }
 }
